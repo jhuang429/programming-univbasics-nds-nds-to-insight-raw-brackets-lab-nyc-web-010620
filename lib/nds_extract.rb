d526a07601dc6b53ba141b63d68e0aff04efe77d
Jerry Huang <jhuang9240@gmail.com>
2019-10-23 00:51:46 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,11 +4,6 @@ require 'directors_database'
 # pp directors_database
 
 def directors_totals(nds)
-  # Remember, it's always OK to pretty print what you get *in* to make sure
-  # that you know what you're starting with!
-  #
-  #
-  # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   result = {
   }
   director_count = 0
